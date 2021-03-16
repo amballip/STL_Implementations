bbfc3a00eac062352f2719d92f1c843ffe87e296
amballip <amballip@gmail.com>
2021-03-16 23:31:42 +0530

    iterator functionality added

--- array/array.hpp
+++ array/array.hpp
@@ -1,15 +1,11 @@
 #include<initializer_list>
 
 template <typename T , size_t _size>
-class array{
+class array
+{
 
 	private:
 	T arr[_size];
-
-	void allocate()
-	{
-	
-	} 
 
 	public:
 
@@ -17,32 +13,57 @@ class array{
 	// array<int ,10> arr;
 	array()
 	{
-	};
+	}
 
 	// constructor with initializer list
 	// array <int , 10> arr = {0,1,2,3,4,5,6,7,8,9};
-	// array <int , 10> arr ({0,1,2,3,4,5,6,7,8,9});
-	// 
+	// array <int , 10> arr1 ({0,1,2,3,4,5,6,7,8,9});
+	// array <int , 10> arr1 ({0,1,2,3,4});  - sets arr1[5] to arr1[9] = 0 or int()
 	array(std::initializer_list<T> arrList)
 	{
 
-		size_t i = 0;
-		for(typename std::initializer_list<T>::iterator it = arrList.begin();it!=arrList.end();it++)
+		if(arrList.size() == _size)
 		{
-			arr[i]=*it;
-			
-			if(i == _size-1)
-				break;
-
-			i++;
+			size_t i = 0;
+			for(typename std::initializer_list<T>::iterator it = arrList.begin();it!=arrList.end();it++)
+			{
+				arr[i]=*it;
+				i++;
+			}
 		}
-	};
+		else if(arrList.size()<_size)
+		{
+			size_t tempIndex = 0;
+			typename std::initializer_list<T>::iterator it = arrList.begin();
+
+			while(tempIndex !=_size)
+			{
+				if(it!=arrList.end())
+				{
+					arr[tempIndex] = *it;
+					tempIndex++;
+					it++;
+				}
+				else
+				{
+					arr[tempIndex] = T();
+					tempIndex++;
+				}
+			}
+		}
+
+		else
+		{
+			constexpr bool i = _size<arrList.size();
+			static_assert(i,"too many arguments");
+		}
+	}
 
 	// destructor
 	~array()
 	{
 
-	};
+	}
 
 	// returns size of the array
 	// size_t a = arr.size();
@@ -67,6 +88,71 @@ class array{
 	{
 		return arr[index];
 	}
+
+	struct iterator
+	{
+
+		iterator(T* ptr = nullptr):m_ptr(ptr)
+		{
+
+		}
+
+		T& operator*() const
+		{
+			return *m_ptr;
+		}
+
+		T& operator->()
+		{
+			return  m_ptr;
+		}
+
+		iterator& operator++()
+		{
+			m_ptr++;
+			return *this;
+		}
+
+		iterator operator++(int)
+		{
+			iterator temp = *this;
+			++(*this);
+			return temp;
+		}
+
+		iterator operator+(size_t offset)
+		{
+			return iterator(m_ptr+offset);
+		}
+
+		bool operator==(const iterator& a)
+		{
+			return this->m_ptr == a.m_ptr;
+		}
+
+		bool operator != (const iterator& a)
+		{
+			return this->m_ptr != a.m_ptr;
+		}
+
+
+
+		private:
+			T* m_ptr;
+
+	};
+
+	iterator begin()
+	{
+		return iterator(&arr[0]);
+	}
+
+	iterator end()
+	{
+		return iterator(&arr[_size]);
+	}
+
+
 
 
 };
