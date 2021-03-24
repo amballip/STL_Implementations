38b33080e9ae5bb0248327e373660343bb3b6158
amballip <amballip@gmail.com>
2021-03-24 13:56:09 +0530

    operator overloading defined

--- forward_list/forward_list.hpp
+++ forward_list/forward_list.hpp
@@ -1,5 +1,7 @@
 #include<memory>
 #include<iostream>
+#include<initializer_list>
+#include<algorithm>
 
 template<typename T>
 struct node
@@ -15,28 +17,129 @@ struct node
 	{
 	}
 
-//	bool operator==(std::shared_ptr<node<T>> lhs)
-//	{
-//		return this->m_value == lhs->m_value;
-//	}
-
 };
 
 template<typename T>
 class forward_list
 {
 	private:
+	// head node. singly linked list starts with head
 	std::shared_ptr<node<T>> head;
-
+	
 	public:
 
+	//iterator declaration
+	struct iterator
+	{
+		private:
+		std::shared_ptr<node<T>> m_data;
 
+
+		public:
+
+		//no default constructor. iterator must be initialized.
+		iterator(std::shared_ptr<node<T>> current):m_data(current)
+		{
+		}
+
+		//++ overload for incrementing in for loops
+		iterator operator++()
+		{
+			 this->m_data = this->m_data->m_next;
+			 return *this;
+		}
+
+		T& operator*()
+		{
+			return (this->m_data->m_value);
+		}
+
+		bool operator!=(iterator rhs)
+		{
+			return !(this->m_data == rhs.m_data);
+		}
+
+		iterator& operator = (iterator& lhs)
+		{
+			this->m_data=lhs.m_data;
+			return *this;
+		}
+
+		std::shared_ptr<node<T>> get()
+		{
+			return m_data;
+		}
+
+	};
+
+	// default constructor
+	// forward_list<int> listA;
 	forward_list():head(nullptr)
 	{
 
 	}
+
+
+	forward_list(forward_list& forwList)
+	{
+		*this = forwList;
+
+	}
+
+	forward_list(size_t count,T elem = T())
+	{
+		head = std::make_shared<node<T>>();
+		head->m_value = elem;
+		iterator it = begin();
+		--count;
+		while(count--)
+		{
+			insert_after(it,elem);
+			++it;
+		}
+
+	}
+
+
+	forward_list(std::initializer_list<T> initList)
+	{
+		head = std::make_shared<node<T>>();
+		typename std::initializer_list<T>::iterator it= initList.begin();
+		head->m_value = *it;
+		++it;
+		iterator thisIt = iterator(head);
+
+		while(it!=initList.end())
+		{
+			insert_after(thisIt,*it);
+			++it;
+			++thisIt;
+		}
+
+
+	}
+
+	forward_list(iterator& itA,iterator& itB)
+	{
+		head = std::make_shared<node<T>>();
+		head->m_value = *itA;
+
+		iterator itRhs(this->head);
+		iterator itLhs = itA.get()->m_next;
+		
+		while(itLhs!=itB)
+		{
+			this->insert_after(itRhs,itLhs.get()->m_value);
+			++itRhs;
+			++itLhs;
+		}
+	}
+
+
+	
 	~forward_list()
 	{
+
 	}
 
 
@@ -74,43 +177,13 @@ class forward_list
 
 	}
 
-
-	struct iterator
+	void insert_after(iterator& it,T elem)
 	{
-		private:
-		std::shared_ptr<node<T>> m_data;
+		std::shared_ptr<node<T>> temp = std::make_shared<node<T>>();
+		temp->m_value = elem;
 
-		public:
-		iterator(std::shared_ptr<node<T>> current):m_data(current)
-		{
-		}
-
-		iterator operator++()
-		{
-			 this->m_data = this->m_data->m_next;
-			 return *this;
-		}
-
-		T& operator*()
-		{
-			return (this->m_data->m_value);
-		}
-
-		bool operator!=(iterator rhs)
-		{
-			return !(this->m_data == rhs.m_data);
-		}
-
-		iterator& operator = (iterator& lhs)
-		{
-			this->m_data=lhs.m_data;
-			return *this;
-		}
-
-	
-	
-	};
-
+		it.get()->m_next = temp;
+	}
 
 	iterator begin()
 	{
@@ -123,9 +196,26 @@ class forward_list
 		return iterator(nullptr);
 	}
 
+	forward_list& operator=(forward_list& rhs)
+	{
+		this->head = std::make_shared<node<T>>();
+		this->head->m_value = rhs.head->m_value;
+
+		iterator lhsIt(this->head);
+		iterator rhsIt(rhs.head->m_next);
+
+		while(rhsIt!=rhs.end())
+		{
+			this->insert_after(lhsIt,rhsIt.get()->m_value);
+			++lhsIt;
+			++rhsIt;
+		}
+
+		return *this;
+
+	}
 
 
-			
 
 };
 
